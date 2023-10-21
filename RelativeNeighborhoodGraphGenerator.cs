794ef18c755747826888304ae1b9c76a4d7ee779
xormarcos <marcosgomes1988@gmail.com>
2023-10-22 00:26:32 +0100

    Separated Vector2 in to different file

--- RelativeNeighborhoodGraphGenerator.cs
+++ RelativeNeighborhoodGraphGenerator.cs
@@ -48,20 +48,3 @@ public static class RelativeNeighborhoodGraphGenerator
         return result;
     }
 }
-
-public struct Vector2
-{
-    public readonly int x;
-    public readonly int y;
-
-    public Vector2(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double DistanceTo(Vector2 vector2)
-    {
-        return Math.Sqrt(Math.Pow(vector2.x - x, 2) + Math.Pow(vector2.y - y, 2));
-    }
-}
