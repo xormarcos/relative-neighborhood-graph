a4cb2f5e83ea3d8b3ec0463c4adf7cd6f2b62137
xormarcos <marcosgomes1988@gmail.com>
2023-11-17 19:16:17 +0100

    Use of parallel foreach

--- RelativeNeighborhoodGraphGenerator.cs
+++ RelativeNeighborhoodGraphGenerator.cs
@@ -1,78 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Threading.Tasks;
 
 namespace RelativeNeighborhoodGraph;
 
 public static class RelativeNeighborhoodGraphGenerator
 {
-    private static Dictionary<string, float> cachedDistances = new();
-
     public static List<Tuple<Vector2, Vector2>> Generate(IEnumerable<Vector2> points)
     {
         List<Tuple<Vector2, Vector2>> result = new();
 
-        foreach (Vector2 point in points)
+        Parallel.ForEach(points, point =>
         {
-            foreach (Vector2 point2 in points)
+            Parallel.ForEach(points, point2 =>
             {
                 if (point == point2 || result.Contains(new Tuple<Vector2, Vector2>(point2, point)))
                 {
-                    continue;
+                    return;
                 }
 
-                float distance = GetDistance(point, point2);
+                float distance = Vector2.DistanceSquared(point, point2);
                 bool closest = true;
 
-                foreach (Vector2 point3 in points)
+                Parallel.ForEach(points, (point3, state) =>
                 {
                     if (point3 == point || point3 == point2)
                     {
-                        continue;
+                        return;
                     }
 
-                    float distance2 = GetDistance(point3, point);
-                    float distance3 = GetDistance(point3, point2);
+                    float distance2 = Vector2.DistanceSquared(point3, point);
+                    float distance3 = Vector2.DistanceSquared(point3, point2);
 
                     if (distance > distance2 && distance > distance3)
                     {
                         closest = false;
-                        break;
+                        state.Break();
                     }
-                }
+                });
 
                 if (closest)
                 {
                     result.Add(new Tuple<Vector2, Vector2>(point, point2));
                 }
-            }
-        }
+            });
+        });
 
         return result;
-    }
-
-    private static float GetDistance(Vector2 point1, Vector2 point2)
-    {
-        string key = point1.ToString() + "/" + point2.ToString();
-
-        float distance;
-
-        if (cachedDistances.TryGetValue(key, out distance))
-        {
-            return distance;
-        }
-
-        string alernativeKey = point2.ToString() + "/" + point1.ToString();
-
-        if (cachedDistances.TryGetValue(alernativeKey, out distance))
-        {
-            return distance;
-        }
-
-        distance = Vector2.DistanceSquared(point1, point2);
-
-        cachedDistances.Add(key, distance);
-
-        return distance;
     }
 }
